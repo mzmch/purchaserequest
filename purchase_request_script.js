8eb86f77f5e1fa0f27133ce609712a6ddb0554f8
mzmch <it@mountzionmedicalcollege.com>
2025-04-17 20:33:55 +0530

    Update purchase_request_script.js

--- purchase_request_script.js
+++ purchase_request_script.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         console.log('Data being sent:', data); // Log the data before sending
 
-        const scriptURL = 'https://script.google.com/macros/s/AKfycby2n0CQOJvxXq2Nc8JJt28DqrrXfr-mOB96pNpJWO88pS7MiQcRub3v9GKqMzcsQtUH/exec'; // Ensure this is your correct script URL
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbzFgc-wZfXDbcpGDsiSuaVJYihNLgbmxc7bigISEEHzBxFQI315FwIlPc-qje-xaL6h/exec'; // Ensure this is your correct script URL
 
         fetch(scriptURL, {
             method: 'POST',
